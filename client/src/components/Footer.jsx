5554ea918d08b2901c2627c13fe902cea5c641ce
Chelsea Yao <szyao416@gmail.com>
2024-02-16 16:19:32 -0500

    added footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-customRed text-white text-center p-4">
+      © MTL Dynamique 2024 | <a href="/privacy-policy" className="">Privacy Policy</a> | <a href="/terms-of-use" className="">Terms of Use</a>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Homepage = () => {
   return (
     <div className="flex flex-row h-screen">
-      <div className="fixed top-0 left-0 w-full h-[35px]">
+      <div className="fixed top-0 w-full h-[35px]">
         <Navbar />
       </div>
       
-      
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
     </div>
   );
 }
