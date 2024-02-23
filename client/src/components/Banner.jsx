0637c9b25d2cb919559c38401419aaebe55a9f0e
Chelsea Yao <szyao416@gmail.com>
2024-02-22 23:53:14 -0500

    Added banner carousel

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -2,22 +2,45 @@ import React from "react";
 import LatestNews from "./BN/LatestNews";
 import ClubName from "./BN/ClubName";
 import backgroundImage from "../assets/banner1.png";
+import Carousel from "nuka-carousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const Banner = () => {
+  const images = [
+    { id: 1, url: backgroundImage },
+    { id: 2, url: backgroundImage },
+    { id: 3, url: backgroundImage },
+  ];
+
+  const nextButtton = <FontAwesomeIcon icon={faChevronRight} />
+  const prevButton = <FontAwesomeIcon icon={faChevronLeft} />
   return (
-    <div
-      className="flex lg:justify-end bg-cover bg-no-repeat w-full h-96"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="flex flex-col p-12 gap-4 lg:w-1/2">
-        <div className="hidden lg:block">
-          <ClubName />
-        </div>
-        <div>
-          <LatestNews />
-        </div>
-      </div>
-    </div>
+    <>
+      <Carousel
+        adaptiveHeight={true}
+        autoplay={true}
+        wrapAround={true}
+        className="w-full"
+        defaultControlsConfig={{ nextButtonText: nextButtton, prevButtonText: prevButton }}
+      >
+        {images.map((image) => (
+          <div
+            className="flex lg:justify-end bg-cover bg-no-repeat w-full h-auto"
+            style={{ backgroundImage: `url(${image.url})` }}
+          >
+            <div className="flex flex-col p-12 gap-4 lg:w-1/2">
+              <div className="hidden lg:block">
+                <ClubName />
+              </div>
+              <div>
+                <LatestNews />
+              </div>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </>
   );
 };
 
