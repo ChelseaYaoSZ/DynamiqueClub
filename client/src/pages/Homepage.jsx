cc5593d7cd9f1cd26544500ae066239eeb49aabc
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-02-23 12:10:11 -0500

    add parent div to HomePage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -3,16 +3,14 @@ import React from "react";
 import Sidebar from "../components/Nav/Sidebar";
 import MeetTheTeams from "../components/MeetTheTeams";
 
-
 import Banner from "../components/Banner";
-
 
 const Homepage = () => {
   return (
-    <>
+    <div className="w-full flex flex-col justify-center items-center">
       <Banner />
       <MeetTheTeams />
-    </>
+    </div>
   );
 };
 
