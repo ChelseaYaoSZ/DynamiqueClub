b7e017e29b6afd60f6128e49d738f474689ce5cd
Chelsea Yao <szyao416@gmail.com>
2024-02-22 23:52:49 -0500

    Refactored to use Outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Newsletter from "./components/Newsletter";
+
 import ContactPage from "./pages/ContactPage";
 import ProgramPage from "./pages/ProgramPage";
+import Page from "./pages/Page";
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col h-screen">
-        <Navbar />
-
-        <div className="flex flex-1">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/program" element={<ProgramPage />} />
-          </Routes>
-        </div>
-
-        <Newsletter />
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Page />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/program" element={<ProgramPage />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
