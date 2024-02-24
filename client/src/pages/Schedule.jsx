b87b5daca2a3cf6a9bf0947c394b9fa6227b90bc
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-02-23 20:09:20 -0500

    update calendar link

--- client/src/pages/Schedule.jsx
+++ client/src/pages/Schedule.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Notification from '../components/Notification';
+import React from "react";
+import Notification from "../components/Notification";
 
 const Schedule = () => {
-  
-  const calendarSrc = "https://calendar.google.com/calendar/embed?src=volleydynamique%40gmail.com&ctz=America%2FToronto";
+  const calendarSrc =
+    "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FToronto&bgcolor=%23ffffff&showPrint=0&showTitle=0&showTz=0&showCalendars=0&showTabs=0&src=dm9sbGV5ZHluYW1pcXVlQGdtYWlsLmNvbQ&color=%23039BE5";
 
   return (
     <div className="flex flex-col justify-center items-center p-20 gap-4">
-      <h2 className="text-4xl font-semibold text-center mb-6">PRACTICE & GAME SCHEDULE</h2>
-      <Notification className=""/>
+      <h2 className="text-4xl font-semibold text-center mb-6">
+        PRACTICE & GAME SCHEDULE
+      </h2>
+      <Notification className="" />
       <iframe
         src={calendarSrc}
-        className="w-3/4 h-[600px] border-0" 
+        className="w-3/4 h-[600px] border-0"
         frameborder="0"
         scrolling="no"
       ></iframe>
