09a2839c912fb60c86ebbc4fb5dd1dbbd989acf0
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-02-28 12:59:52 -0500

    add animation for TeamCard

--- client/src/components/MeetTeams/TeamCard.jsx
+++ client/src/components/MeetTeams/TeamCard.jsx
@@ -6,15 +6,18 @@ const TeamCard = ({ data }) => {
     <div className="relative w-[180px] h-[249px] card:w-[190px] card:h-[250px] sm:w-[270px] sm:h-[380px] md:w-[300px] md:h-[415px] xl:w-[324px] xl:h-[450px] mb-2  sm:border-spacing-x-20 border-12 sm:border-15 border-primary shadow-md shadow-slate-300">
       <div className="w-full flex flex-col justify-center items-center">
         <img src={data.image} alt="team" />
-        <div className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-14  sm:-translate-y-24">
-          <p className="text-white text-xs sm:text-2xl font-jetBrains font-extrabold transform translate-y-7 sm:translate-y-10">
-            UNDER
-          </p>
-          <h2 className="text-customYellow text-5xl my-5 sm:text-110 font-monomaniac text-center">
-            {data.highlighter}
-          </h2>
-          <div className="w-[80px] h-[7px] sm:w-[161px] sm:h-[14px] text-[1px] rounded-md bg-customRed text-customRed transform -translate-y-3">
-            red
+        <div className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-14  sm:-translate-y-24 group ease-in-out">
+          <div className="flex flex-col justify-center items-center transition-all group-hover:-translate-y-2">
+            <p className="text-white text-xs sm:text-2xl font-jetBrains font-extrabold transform translate-y-7 sm:translate-y-10">
+              UNDER
+            </p>
+            <h2 className="text-customYellow text-5xl my-5 sm:text-110 font-monomaniac text-center">
+              {data.highlighter}
+            </h2>
+          </div>
+
+          <div className="w-[80px] h-[7px] sm:w-[161px] sm:h-[14px] text-[8px] font-bold rounded-md bg-customRed text-customRed transform -translate-y-3 group-hover:h-[15px] sm:group-hover:h-[20px] sm:text-sm text-center  group-hover:text-white">
+            <p className="">MEET THE TEAM</p>
           </div>
         </div>
       </div>
