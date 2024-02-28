0149b8a6162b8d6835654ad2c072003b4a87f836
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-02-28 13:27:55 -0500

    update animation

--- client/src/components/MeetTeams/TeamCard.jsx
+++ client/src/components/MeetTeams/TeamCard.jsx
@@ -6,7 +6,7 @@ const TeamCard = ({ data }) => {
     <div className="relative w-[180px] h-[249px] card:w-[190px] card:h-[250px] sm:w-[270px] sm:h-[380px] md:w-[300px] md:h-[415px] xl:w-[324px] xl:h-[450px] mb-2  sm:border-spacing-x-20 border-12 sm:border-15 border-primary shadow-md shadow-slate-300">
       <div className="w-full flex flex-col justify-center items-center">
         <img src={data.image} alt="team" />
-        <div className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-14  sm:-translate-y-24 group ease-in-out">
+        <div className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-14  sm:-translate-y-24 group ease-in-out duration-700 delay-150">
           <div className="flex flex-col justify-center items-center transition-all group-hover:-translate-y-2">
             <p className="text-white text-xs sm:text-2xl font-jetBrains font-extrabold transform translate-y-7 sm:translate-y-10">
               UNDER
