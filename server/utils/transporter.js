0b7df994236aa5fba4e48b9aed2b92f835fe5463
Chelsea Yao <szyao416@gmail.com>
2024-02-28 22:42:56 -0500

    Simplify transporter config

--- server/utils/transporter.js
+++ server/utils/transporter.js
@@ -12,9 +12,6 @@ let transporter;
 try {
   transporter = createTransport({
     service: "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
     auth: {
       user: process.env.USER_EMAIL_ACCOUNT,
       pass: process.env.USER_EMAIL_PASSWORD,
