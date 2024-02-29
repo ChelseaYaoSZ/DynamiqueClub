ae2eb675cfd4f88df4a6c519c5dad221930bb116
Chelsea Yao <szyao416@gmail.com>
2024-02-28 21:58:12 -0500

    Refactored api call

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PlayerInfo from "../components/Registration/PlayerInfo";
 import ParentInfo from "../components/Registration/ParentInfo";
 import Waiver from "../components/Registration/Waiver";
+import { sendEmail } from "../utils/sendEmail";
 
 const ResgistrationPage = () => {
   const handleSubmit = async (event) => {
@@ -10,30 +11,31 @@ const ResgistrationPage = () => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
     console.log(data);
-
-    const response = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      alert('Form submitted successfully');
-    } else {
-      alert('Failed to submit form');
+    
+    try {
+      const response = await sendEmail(data);
+      if (response.success) {
+        alert(response.message);
+      } else {
+        alert(response.message);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      alert('An error occurred while submitting the form. Please try again later.');
     }
-
   };
-  
+
   return (
     <div className="flex flex-col justify-center items-center py-8 lg:p-20 gap-4">
       <div className="text-3xl lg:text-4xl font-semibold text-center lg:mb-6">
         <h2>Registration Form</h2>
       </div>
       <div className="flex flex-col gap-4 lg:gap-8 max-w-[950px] bg-bgWhite p-10">
-        <form onSubmit={handleSubmit} id="registration-form" className="flex flex-col gap-4 lg:gap-8">
+        <form
+          onSubmit={handleSubmit}
+          id="registration-form"
+          className="flex flex-col gap-4 lg:gap-8"
+        >
           <PlayerInfo />
           <ParentInfo />
           <Waiver />
