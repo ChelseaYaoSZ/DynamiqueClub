8591fe842b27745959ac612c72d41bd49fcaee59
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-03-05 20:26:56 -0500

    add Program sub-links

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const programs = [
+  { name: "U17 Competitive", path: "/program/u17" },
+  { name: "U16 Competitive", path: "/program/u16" },
+  { name: "U14 Competitive", path: "/program/u14" },
+  { name: "U13 Competitive", path: "/program/u13" },
+  { name: "DEV1", path: "/program/dev1" },
+  { name: "DEV2", path: "/program/dev2" },
+];
+
 const Nav = ({ isDropdown = false }) => {
   const navLinks = [
     { name: "Home", path: "/" },
-    { name: "Program", path: "/program" },
+    { name: "Program", path: "/program", dropdown: true },
     { name: "Registration", path: "/registration" },
     { name: "Schedule", path: "/schedule" },
     { name: "About Us", path: "/club" },
@@ -20,22 +29,36 @@ const Nav = ({ isDropdown = false }) => {
   return (
     <nav className={`bg-customRed flex ${containerClasses} p-3 text-xl h-full`}>
       {navLinks.map((link) => (
-        <NavLink
-          key={link.name}
-          to={link.path}
-          className={({ isActive }) =>
-            `${linkClasses} ${
-              isActive
-                ? "text-customYellow font-medium text-2xl"
-                : "text-white hover:text-customYellow hover:font-medium"
-            }`
-          }
-          style={({ isActive }) => ({
-            textShadow: isActive ? "0px 1px 0px #ffffff" : "none",
-          })}
-        >
-          {link.name}
-        </NavLink>
+        <div className="group" key={link.name}>
+          <NavLink
+            to={link.path}
+            className={({ isActive }) =>
+              `${linkClasses} ${
+                isActive
+                  ? "text-customYellow font-medium text-2xl"
+                  : "text-white hover:text-customYellow hover:font-medium"
+              }`
+            }
+            style={({ isActive }) => ({
+              textShadow: isActive ? "0px 1px 0px #ffffff" : "none",
+            })}
+          >
+            {link.name}
+          </NavLink>
+          {link.dropdown && (
+            <div className="absolute hidden group-hover:block bg-primary text-black mt-1">
+              {programs.map((program) => (
+                <NavLink
+                  key={program.name}
+                  to={program.path}
+                  className="block px-4 py-2 text-left text-base font-medium hover:bg-gray-200 hover:text-customRed"
+                >
+                  {program.name}
+                </NavLink>
+              ))}
+            </div>
+          )}
+        </div>
       ))}
     </nav>
   );
