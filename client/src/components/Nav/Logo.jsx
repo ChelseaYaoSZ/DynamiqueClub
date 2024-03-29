93c751413df73a5755093d8a84584748bdfeaf8f
Chelsea Yao <szyao416@gmail.com>
2024-03-29 13:35:21 -0400

    updated logo with link

--- client/src/components/Nav/Logo.jsx
+++ client/src/components/Nav/Logo.jsx
@@ -1,10 +1,11 @@
-// Import React and your logo image
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import logoImage from '../../assets/logo-transparent.png'; 
 
 const Logo = () => {
   return (
-    <div className="flex h-full lg:justify-center items-center pt-2  pl-2 sm:pl-12 lg:pl-0 bg-primary">
+    // Use Link component to wrap the entire content, directing to the homepage ("/")
+    <Link to="/" className="flex h-full lg:justify-center items-center pt-2 pl-2 sm:pl-12 lg:pl-0 bg-primary no-underline">
       
       <img src={logoImage} alt="Logo" className="h-14 mr-1 ml-2" /> 
       
@@ -12,7 +13,7 @@ const Logo = () => {
       
       <span style={{ textShadow: "1.5px 0px 0px #C00021", marginLeft: '-6px' }} className="text-black font-semibold text-3xl">Dynamique</span>
 
-    </div>
+    </Link>
   );
 };
 
