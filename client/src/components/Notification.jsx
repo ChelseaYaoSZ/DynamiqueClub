96ae3748438d5058d19448d5039d88a604692b4b
Chelsea Yao <szyao416@gmail.com>
2024-06-21 09:52:07 -0400

    fr version for schedule page

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
@@ -6,22 +7,18 @@ import useFetchNotes from "../hooks/useFetchNotes";
 import moment from "moment";
 
 const Notification = () => {
+  const { t } = useTranslation();
   const { notes, loading, error } = useFetchNotes();
 
-  const [updatedDate, setUpdatedDate] = useState("");
-  const [note, setNote] = useState("");
+  const isNotesAvailable = notes && notes.length > 0;
+  const [firstNote, setFirstNote] = useState("No notes available.");
   const [buttonDisplay, setButtonDisplay] = useState(false);
 
   useEffect(() => {
-    if (notes && notes.length > 0) {
+    if (isNotesAvailable) {
       console.log("Notes fetched successfully:", notes);
-      setUpdatedDate(moment(notes[0].updatedAt).format("MMMM Do YYYY"));
-      setNote(notes[0].note);
+      setFirstNote(notes[0].note);
       setButtonDisplay(notes[0].buttonDisplay);
-    } else {
-      setUpdatedDate(moment().format("MMMM Do YYYY"));
-      setNote("No notes available.");
-      setButtonDisplay(false);
     }
   }, [notes]);
 
@@ -32,16 +29,16 @@ const Notification = () => {
     <div className="bg-bgWhite text-black py-3 px-6 text-lg w-3/4 border">
       <div className="flex gap-4">
         <div className="bg-customRed text-sm text-white px-2 py-0 rounded font-medium">
-          NOTE IMPORTANT!
+          {t("common.schedule.subtitle")}
         </div>
-        <div className="text-sm">{updatedDate}</div>
+        <div className="text-sm">{isNotesAvailable ? moment(notes[0].updatedAt).format("MMMM Do YYYY") : ""}</div>
       </div>
 
       <div className="w-full text-start text-xl font-monomaniac text-customRed pt-4 scroll-text">
         <span className="scroll-animation-lg whitespace-pre-line">
           {" "}
           <FontAwesomeIcon icon={faBullhorn} className="mr-2" />
-          {note}
+          {firstNote}
         </span>
       </div>
       <div className="flex justify-end pt-4">
@@ -50,7 +47,7 @@ const Notification = () => {
             to="/registration"
             className="bg-customRed text-white text-base font-medium rounded px-2 py-1 hover:font-bold hover:bg-customBlue"
           >
-            Register now
+            {t("common.schedule.button")}
           </Link>
         )}
       </div>
--- client/src/pages/SchedulePage.jsx
+++ client/src/pages/SchedulePage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Notification from "../components/Notification";
 
 const SchedulePage = () => {
-
+  const { t } = useTranslation();
   const calendarSrc = "https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=America%2FToronto&bgcolor=%23ffffff&showTitle=0&showTz=0&src=dm9sbGV5ZHluYW1pcXVlQGdtYWlsLmNvbQ&color=%23039BE5";
 
   return (
     <div className="flex flex-col justify-center items-center py-8 lg:p-20 gap-4">
-      <h2 className="text-3xl lg:text-4xl font-semibold text-center lg:mb-6">PRACTICE & GAME SCHEDULE</h2>
+      <h2 className="text-3xl lg:text-4xl font-semibold text-center lg:mb-6">{t('common.schedule.title')}</h2>
       <Notification/>
       <iframe
         id="calendar"
